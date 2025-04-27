26d1811bed572a8acc0f02c7b5cccd588e6b7526
Learnli23 <learnli759@gmail.com>
2025-04-27 14:40:33 +0300

    Update settings.py

--- aisha/settings.py
+++ aisha/settings.py
@@ -89,6 +89,15 @@ WSGI_APPLICATION = 'aisha.wsgi.application'
 
 '''
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+'''
+
+DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
@@ -96,14 +105,8 @@ DATABASES = {
     )
 }
 
-'''
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
