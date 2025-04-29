fd3f397050bc2df69050b69927e634449721b884
Learnli23 <learnli759@gmail.com>
2025-04-29 10:20:23 +0300

    Update apps.py

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+    name = 'app'  #  app's  name
+
+    def ready(self):
+        import app.signals  # This ensures signals.py is imported on app startup
