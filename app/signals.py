897c1fdae1095ac1614859ab7d1da8b543f68c61
Learnli23 <learnli759@gmail.com>
2025-05-07 17:17:20 +0300

    Update signals.py

--- app/signals.py
+++ app/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Student, AcademicRecord
-from .analysis import run_attrition_analysis
+from .annalysis import run_attrition_analysis
 import datetime
 
 @receiver(post_save, sender=Student)
