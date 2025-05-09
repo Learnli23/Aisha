5bcf3690ccce0d696afb57c5c437e503c21ccf3c
Learnli23 <learnli759@gmail.com>
2025-05-09 10:59:10 +0300

    Update annalysis.py

--- app/annalysis.py
+++ app/annalysis.py
@@ -41,13 +41,12 @@ def define_fuzzy_membership_functions():
     Returns dictionaries of fuzzy variables.
     """
     # GPA
-# GPA ranges and fuzzy sets (0–5 scale)
-gpa_range = np.arange(0, 5.1, 0.1)
-gpa_low = fuzz.trimf(gpa_range, [0, 0, 2.8])           # Low GPA ends at 2.8
-gpa_medium = fuzz.trimf(gpa_range, [3, 3.5, 3.9])       # Medium GPA from 3.0 to 3.9
-gpa_high = fuzz.trimf(gpa_range, [4, 5.0, 5.0])         # High GPA from 4.0 to 5.0
-
-
+    # GPA ranges and fuzzy sets (0–5 scale)
+    gpa_range = np.arange(0, 5.1, 0.1)
+    gpa_low = fuzz.trimf(gpa_range, [0, 0, 2.8])           # Low GPA ends at 2.8
+    gpa_medium = fuzz.trimf(gpa_range, [3, 3.5, 3.9])       # Medium GPA from 3.0 to 3.9
+    gpa_high = fuzz.trimf(gpa_range, [4, 5.0, 5.0])         # High GPA from 4.0 to 5.0
+    
 
     # Financial status (we assign scores: struggling = 2, good = 5, scholarship = 8)
     finance_range = np.arange(0, 11, 1)
