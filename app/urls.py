92a79bb251f4bf66efa572f1ee5fd9a1960ec9e5
Learnli23 <learnli759@gmail.com>
2025-05-14 14:49:24 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('analysis_results/', views.view_analysis_results, name='view_analysis_results'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('attrition_dashboard/', views.risk_level_distribution, name='attrition_dashboard'),
+    path('login/', views.admin_login, name='admin_login'),
 
 
 ]
