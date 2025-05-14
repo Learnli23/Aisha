3e3f90f9edd732f737901e6355083a7e86ac73c2
Learnli23 <learnli759@gmail.com>
2025-05-14 17:01:20 +0300

    Update settings.py

--- aisha/settings.py
+++ aisha/settings.py
@@ -197,7 +197,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGOUT_REDIRECT_URL = 'login'  # or any other URL name
+LOGOUT_REDIRECT_URL = 'home'  # or any other URL name
 
 
 
