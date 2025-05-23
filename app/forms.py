f6d6c340a2b859fea160d20eab78d5c46cc87992
Learnli23 <learnli759@gmail.com>
2025-05-23 09:30:28 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,8 +3,8 @@ from django import forms
 class AdminLoginForm(forms.Form):
     username = forms.CharField(
         max_length=150,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'})
+        widget=forms.TextInput(attrs={'class': 'form-control'})
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
     )
